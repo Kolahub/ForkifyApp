b335a6531000e2c238ed18ca3744ad6a851d33dd
Ayofe Faheez <ayofefaheez@gmail.com>
2024-04-15 11:17:11 -0800

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '28b31f20-2748-4c71-a213-7ee955271179';
+export const MODAL_CLOSE_SEC = 2.5;
